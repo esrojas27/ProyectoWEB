fe6d14d3c3f0462c43405667b36dada17280a17b
esrojas27 <esrojas27@gmail.com>
2022-09-06 19:39:40 -0500

    Listar panteras

--- cliente/src/app/components/list-panteras/list-panteras.component.ts
+++ cliente/src/app/components/list-panteras/list-panteras.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pantera } from 'src/app/models/pantera';
+import { PanteraService } from 'src/app/service/pantera.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list-panteras',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPanterasComponent implements OnInit {
 
-  constructor() { }
+  panteras: Pantera[] = [];
+
+  constructor(private panteraService:PanteraService) { }
 
   ngOnInit(): void {
+    this.cargarPanteras()
+  }
+
+  cargarPanteras(){
+    this.panteraService.lista().subscribe({
+      next:res=>{
+        this.panteras=res;
+        console.log(this.panteras);
+      },
+      error:(err)=>console.log(err)
+    })
+  }
+
+  borrar(id: any) {
+    Swal.fire({
+      title: 'Desea eliminar la pantera',
+      icon: 'warning',
+      showCloseButton:true,
+      confirmButtonText:"Aceptar",
+      confirmButtonColor: "#DD6B55",
+    })
+    .then((result)=>{
+      if(result.value){
+        this.panteraService.delete(id).subscribe({
+          next:res=>{
+            this.cargarPanteras();
+            Swal.fire({
+              title: 'Pantera Eliminada',
+              icon: 'success',
+              showCloseButton:true,
+              confirmButtonText:"Aceptar",
+              confirmButtonColor: "#DD6B55",
+            });
+          }
+        });
+      }
+    });
   }
 
 }
