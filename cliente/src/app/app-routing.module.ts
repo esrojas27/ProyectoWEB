616a8f2f1f50800fbb4fbfbebfddd1d76bfdec0a
esrojas27 <esrojas27@gmail.com>
2022-09-06 19:00:55 -0500

    Rutas

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetallePanteraComponent } from './components/detalle-pantera/detalle-pantera.component';
+import { EditarPanteraComponent } from './components/editar-pantera/editar-pantera.component';
+import { ListPanterasComponent } from './components/list-panteras/list-panteras.component';
+import { NuevaPanteraComponent } from './components/nueva-pantera/nueva-pantera.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:ListPanterasComponent
+  },
+  {
+    path:'detalle/:id',
+    component:DetallePanteraComponent
+  },
+  {
+    path:'nuevo',
+    component:NuevaPanteraComponent
+  },
+  {
+    path:'editar/:id',
+    component:EditarPanteraComponent
+  },
+  {
+    path:'**',
+    redirectTo:'',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
