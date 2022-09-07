1a73f9ea2f13bae56d2ed8884a29af8a27b8d86b
esrojas27 <esrojas27@gmail.com>
2022-09-06 19:55:33 -0500

    crear y editar panteras

--- cliente/src/app/components/editar-pantera/editar-pantera.component.ts
+++ cliente/src/app/components/editar-pantera/editar-pantera.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pantera } from 'src/app/models/pantera';
+import { PanteraService } from 'src/app/service/pantera.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-editar-pantera',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarPanteraComponent implements OnInit {
 
-  constructor() { }
+  pantera: Pantera = {};
+  id:string='';
+
+  constructor(private panteraService:PanteraService,private activatedRoute: ActivatedRoute,private router: Router ) { }
 
   ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.panteraService.detail(Number(this.id)).subscribe({
+      next:res=>{
+        this.pantera=res;
+      },
+      error:(err)=>{
+        Swal.fire({
+          title: 'Error en la creacion',
+          text:err,
+          icon: 'error',
+          showCloseButton:true,
+          confirmButtonText:"Aceptar",
+          confirmButtonColor: "#DD6B55",
+        })
+        this.router.navigate(['/']);
+      }
+    })
+  }
+
+  onUpdate(): void {
+    this.panteraService.update(Number(this.id), this.pantera).subscribe({
+      next:res=>{
+        Swal.fire({
+          title: 'Pantera Creada',
+          icon: 'success',
+          showCloseButton:true,
+          confirmButtonText:"Aceptar",
+          confirmButtonColor: "#DD6B55",
+        })
+        this.router.navigate(['/']);
+      },
+      error:(err)=>{
+        Swal.fire({
+          title: 'Error en la creacion',
+          text:err,
+          icon: 'error',
+          showCloseButton:true,
+          confirmButtonText:"Aceptar",
+          confirmButtonColor: "#DD6B55",
+        })
+        this.router.navigate(['/']);
+      }
+      
+    });
   }
 
 }
--- cliente/src/app/components/nueva-pantera/nueva-pantera.component.ts
+++ cliente/src/app/components/nueva-pantera/nueva-pantera.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pantera } from 'src/app/models/pantera';
+import { PanteraService } from 'src/app/service/pantera.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-nueva-pantera',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevaPanteraComponent implements OnInit {
 
-  constructor() { }
+  nombre = '';
+
+  constructor(private panteraService:PanteraService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate(): void {
+    const pantera = new Pantera(this.nombre);
+    this.panteraService.save(pantera).subscribe({
+      next:res=>{
+        Swal.fire({
+          title: 'Pantera Creada',
+          icon: 'success',
+          showCloseButton:true,
+          confirmButtonText:"Aceptar",
+          confirmButtonColor: "#DD6B55",
+        })
+        this.router.navigate(['/']);
+      },
+      error:(err)=>{
+        Swal.fire({
+          title: 'Error en la creacion',
+          text:err,
+          icon: 'error',
+          showCloseButton:true,
+          confirmButtonText:"Aceptar",
+          confirmButtonColor: "#DD6B55",
+        })
+        this.router.navigate(['/']);
+      }
+    });
   }
 
 }
